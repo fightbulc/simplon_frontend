4a926ad68d40d703d389ce52d8dfc98888251abd
Tino Ehrich <ehrich@efides.com>
2012-10-25 13:30:49 +0200

    init

--- src/Simplon/Frontend/Collections.php
+++ src/Simplon/Frontend/Collections.php
@@ -1,11 +1,7 @@
 <?php
-  /**
-   * Created by JetBrains PhpStorm.
-   * User: fightbulc
-   * Date: 22.10.12
-   * Time: 14:41
-   * To change this template use File | Settings | File Templates.
-   */
+
+  namespace Simplon\Frontend;
+
   class Collections
   {
 
