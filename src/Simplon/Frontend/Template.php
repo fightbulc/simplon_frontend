69aa0b21ccfad33df6afebf58a355effca540b76
Tino Ehrich <ehrich@efides.com>
2012-12-03 17:20:57 +0100

    new mustache wrapper

--- src/Simplon/Frontend/Template.php
+++ src/Simplon/Frontend/Template.php
@@ -2,33 +2,28 @@
 
   namespace Simplon\Frontend;
 
+  use Phly\Mustache\Mustache;
+
   class Template
   {
-    /** @var \Mustache_Engine */
+    /** @var \Phly\Mustache\Mustache */
     protected $_mustacheInstance;
-
-    protected $_templateRootPath;
-
-    // ##########################################
-
-    /**
-     * @return Template
-     */
-    public static function init()
-    {
-      return new Template();
-    }
+    protected $_templatePath;
+    protected $_templateSuffix = 'html';
+    protected $_templateName;
+    protected $_templateDataItems = [];
+    protected $_templatePartialAliases = [];
 
     // ##########################################
 
     /**
-     * @return \Mustache_Engine
+     * @return \Phly\Mustache\Mustache
      */
     protected function _getMustacheInstance()
     {
       if(! $this->_mustacheInstance)
       {
-        $this->_mustacheInstance = new \Mustache_Engine();
+        $this->_mustacheInstance = new Mustache();
       }
 
       return $this->_mustacheInstance;
@@ -37,12 +32,12 @@
     // ##########################################
 
     /**
-     * @param $rootPath
+     * @param $templatePath
      * @return Template
      */
-    public function setTemplateRootPath($rootPath)
+    public function setTemplatePath($templatePath)
     {
-      $this->_templateRootPath = rtrim($rootPath, '/');
+      $this->_templatePath = $templatePath;
 
       return $this;
     }
@@ -51,39 +46,168 @@
 
     /**
      * @return mixed
+     * @throws \Exception
      */
-    protected function _getTemplateRootPath()
+    protected function _getTemplatePath()
     {
-      return $this->_templateRootPath;
+      if(empty($this->_templatePath))
+      {
+        throw new \Exception('Simplon\Frontend\Template: missing templatePath', 500);
+      }
+
+      return $this->_templatePath;
     }
 
     // ##########################################
 
     /**
-     * @param $templatePath
-     * @param array $templateVars
+     * @param $templateName
+     * @return Template
+     */
+    public function setTemplate($templateName)
+    {
+      $this->_templateName = $templateName;
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    protected function _getTemplate()
+    {
+      if(empty($this->_templateName))
+      {
+        throw new \Exception('Simplon\Frontend\Template: missing template.', 500);
+      }
+
+      return $this->_templateName;
+    }
+
+    // ##########################################
+
+    /**
+     * @param $suffix
+     * @return Template
+     */
+    public function setTemplateSuffix($suffix)
+    {
+      $this->_templateSuffix = $suffix;
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    protected function _getTemplateSuffix()
+    {
+      if(empty($this->_templateSuffix))
+      {
+        throw new \Exception('Simplon\Frontend\Template: missing templateSuffix (e.g. html)', 500);
+      }
+
+      return $this->_templateSuffix;
+    }
+
+    // ##########################################
+
+    /**
+     * @param $key
+     * @param $value
+     * @return Template
+     */
+    public function addDataItem($key, $value)
+    {
+      $this->_templateDataItems[$key] = $value;
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @param array $items
+     * @return Template
+     */
+    public function setDataItems(array $items)
+    {
+      $this->_templateDataItems = $items;
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @return array
+     */
+    protected function _getDataItems()
+    {
+      return $this->_templateDataItems;
+    }
+
+    // ##########################################
+
+    /**
+     * @param $partialTemplateName
+     * @param $partialAliasName
+     * @return Template
+     */
+    public function addPartialAlias($partialTemplateName, $partialAliasName)
+    {
+      $this->_templatePartialAliases[$partialTemplateName] = $partialAliasName;
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @return array
+     */
+    protected function _getPartialAliases()
+    {
+      return $this->_templatePartialAliases;
+    }
+
+    // ##########################################
+
+    /**
+     * @return bool
+     */
+    protected function _resetData()
+    {
+      $this->_templateName = '';
+      $this->_templateDataItems = [];
+      $this->_templatePartialAliases = [];
+
+      return TRUE;
+    }
+
+    // ##########################################
+
+    /**
      * @return string
      */
-    public function renderTemplate($templatePath, array $templateVars)
+    public function render()
     {
-      $rootPath = $this->_getTemplateRootPath();
-      $cleanedTemplatePath = ltrim($templatePath, '/');
-
-      $template = $this->_fetchTemplate($rootPath . '/' . $cleanedTemplatePath);
-
-      return $this
+      // render template
+      $renderedTemplate = $this
         ->_getMustacheInstance()
-        ->render($template, $templateVars);
-    }
+        ->setSuffix($this->_getTemplateSuffix())
+        ->setTemplatePath($this->_getTemplatePath())
+        ->render($this->_getTemplate(), $this->_getDataItems(), $this->_getPartialAliases());
 
-    // ##########################################
+      // reset template data
+      $this->_resetData();
 
-    /**
-     * @param $templatePath
-     * @return string
-     */
-    protected function _fetchTemplate($templatePath)
-    {
-      return join('', file($templatePath));
+      return $renderedTemplate;
     }
   }
