88869022e38e84c68686c0ba909e303a1ee49db2
Tino Ehrich <ehrich@efides.com>
2015-01-17 17:58:43 +0100

    updated simplon/error error response so we dont mix things up

--- src/Responses/ErrorResponse.php
+++ src/Responses/ErrorResponse.php
@@ -2,6 +2,7 @@
 
 namespace Simplon\Frontend\Responses;
 
+use Simplon\Error\ErrorContext;
 use Simplon\Frontend\Interfaces\ResponseInterface;
 
 /**
@@ -9,6 +10,6 @@ use Simplon\Frontend\Interfaces\ResponseInterface;
  * @package Simplon\Frontend\Responses
  * @author Tino Ehrich (tino@bigpun.me)
  */
-class ErrorResponse extends \Simplon\Error\ErrorResponse implements ResponseInterface
+class ErrorResponse extends ErrorContext implements ResponseInterface
 {
 }
