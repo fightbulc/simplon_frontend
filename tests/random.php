53c60674ddd7f23687e4824748142e22a380ff66
Tino Ehrich <ehrich@efides.com>
2015-04-20 21:19:06 +0200

    emptied random

--- tests/random.php
+++ tests/random.php
@@ -1,21 +1,3 @@
 <?php
 
-use Simplon\Frontend\Frontend;
-use Simplon\Frontend\Utils\SessionStorage;
-
 require __DIR__ . '/../vendor/autoload.php';
-
-$sessionStorage = (new SessionStorage())->start();
-
-Frontend::setSessionStorage($sessionStorage);
-
-if (Frontend::hasFlash())
-{
-    echo '<h3>READ/DEL FLASH...</h3>';
-    var_dump([htmlspecialchars(Frontend::getFlash()), Frontend::hasFlash()]);
-}
-else
-{
-    echo '<h3>SET FLASH...</h3>';
-    Frontend::setFlashSuccess('SUCCESS');
-}
